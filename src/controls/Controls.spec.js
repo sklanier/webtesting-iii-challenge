6fdc7165d7735f1eabe3245c37c251a8a31db2fd
Steve Lanier <stevekentlanier@gmail.com>
2019-05-15 16:03:46 -0700

    Wednesday MVP complete

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,45 @@
 // Test away!
+import React from 'react';
+import { toBeDisabled } from 'jest-dom';
+import { render, fireEvent } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+import Controls from './Controls';
+
+expect.extend({ toBeDisabled });
+
+describe('Control component tests', () => {
+	test('should render button to toggle closed and locked states', () => {
+		const { getByText } = render(<Controls />);
+
+		getByText(/close gate/i);
+		getByText(/lock gate/i);
+	});
+
+	test('should call toggleClosed to change on button click', () => {
+		const toggleClosed = jest.fn();
+		const { getByText } = render(
+			<Controls toggleClosed={toggleClosed} />
+		);
+
+		const button = getByText(/close gate/i);
+		fireEvent.click(button);
+
+		expect(toggleClosed).toBeCalledTimes(1);
+	});
+
+	test('should disable open/close button when gate is locked', () => {
+		const { getByText } = render(<Controls locked />);
+
+		const button = getByText(/close gate/i);
+
+		expect(button).toBeDisabled();
+	});
+
+	test('should disable lock/unlock button when gate is open', () => {
+		const { getByText } = render(<Controls closed={false} />);
+
+		const button = getByText(/lock gate/i);
+
+		expect(button).toBeDisabled();
+	});
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,51 @@
 // Test away!
+import React from 'react';
+import { toHaveClass } from 'jest-dom';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+import 'react-testing-library/cleanup-after-each';
+
+import Display from './Display';
+
+expect.extend({ toHaveClass });
+
+describe('Display component tests', () => {
+	test('should display component', () => {
+		render(<Display />);
+	});
+
+	test('displays unlocked and open by default', () => {
+		const { getByText } = render(<Display />);
+
+		getByText(/unlocked/i);
+		getByText(/open/i);
+	});
+
+	test('displays Closed if props.closed is true', () => {
+		const { getByText } = render(<Display closed={true} />);
+
+		getByText(/closed/i);
+	});
+
+	test('displays Locked if props.locked is true', () => {
+		const { getByText } = render(<Display locked={true} />);
+
+		getByText(/locked/i);
+	});
+
+	test('displays red class when props.locked or props.closed are true', () => {
+		const { getByText } = render(<Display locked={true} />);
+
+		const locked = getByText(/locked/i);
+
+		expect(locked).toHaveClass('red-led');
+	});
+
+	test('displays green class when props.unlocked or props.open is true', () => {
+		const { getByText } = render(<Display locked={false} />);
+
+		const locked = getByText(/unlocked/i);
+
+		expect(locked).toHaveClass('green-led');
+	});
+});
